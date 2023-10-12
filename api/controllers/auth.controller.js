25020c7f243e53647bdcf7bd1a2033a8c595b15d
mukesh786yadav <1032190391@tcetmumbai.in>
2023-10-12 22:25:32 +0530

    Added midleware

---
+++ api/controllers/auth.controller.js
@@ -0,0 +1,16 @@
+import User from "../models/user.model.js"
+import bcryptjs from "bcryptjs"
+// import { errorHandler } from "../utils.js/error.js";
+
+export const signup = async (req,res , next)=>{
+    const {username, email, password} =req.body;
+    const hashedPassword = await bcryptjs.hash(password,10)
+    const newUser = new User({username, email, password:hashedPassword});
+    try {
+        await newUser.save()
+        res.status(201).json("user created successfully !...")
+        
+    } catch (error) {
+        next(error);
+    }
+}
--- api/index.js
+++ api/index.js
@@ -1,11 +1,14 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import mongoose from "mongoose";
+import userRouter from "./routes/user.route.js";
+import authRouter from "./routes/auth.route.js"
 
 dotenv.config();
 const db =process.env.MongoURL;
 const po =process.env.PORT;
 const app = express();
+app.use(express.json())
 
 mongoose.connect(db).then( ()=>{
     console.log("Connected to database !...");
@@ -16,6 +19,14 @@ mongoose.connect(db).then( ()=>{
     console.log(err);
 })
 
-
-
-
+app.use('/api/user',userRouter);
+app.use('/api/auth',authRouter)
+app.use((err, req,res,next) =>{
+    const statusCode = err.statusCode || 500;
+    const message = err.message || "internal server error";
+    return res.status(statusCode).json({
+        success:false,
+        statusCode,
+        message
+    });
+})
